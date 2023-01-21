2ed10863e7c6c7b7bb7d0126535f4c3ba9f93d05
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2023-01-21 23:52:31 +0200

    game

---
+++ js/millionaire.js
@@ -0,0 +1,100 @@
+// const URL =
+//   "https://raw.githubusercontent.com/aaronnech/Who-Wants-to-Be-a-Millionaire/master/questions.json";
+const getQuestion = document.querySelector("#question");
+const getAnswer = document.querySelector("#list");
+const submitBtn = document.querySelector("#submit");
+const newGameBtn = document.querySelector("#submit-new");
+
+let questionIndex = 0;
+clearPage();
+showQuestion();
+
+function clearPage() {
+  getQuestion.innerHTML = "";
+  getAnswer.innerHTML = "";
+}
+newGameBtn.onclick = () => history.go();
+
+function showQuestion() {
+  // Question
+  const headerTemplate = `<h2 class="title">%title%</h2>`;
+  const title = headerTemplate.replace(
+    "%title%",
+    questions[questionIndex]["question"]
+  );
+  getQuestion.innerHTML = title;
+  // Answers
+  let answerNumber = 1;
+  for (answerText of questions[questionIndex]["answers"]) {
+    const questionTemplate = `<li>
+          <button value='%number%' onclick = checkAnswer({value}) id="submit" type="submit">
+            <span>%answer%</span>
+          </button>
+        </li>`;
+    const answerHTML = questionTemplate
+      .replace("%answer%", answerText)
+      .replace("%number%", answerNumber);
+    // console.log(answerHTML);
+    getAnswer.innerHTML += answerHTML;
+    answerNumber++;
+  }
+}
+
+function checkAnswer(btn) {
+  const userAnswer = parseInt(btn.value);
+  const rightAnswer = userAnswer === questions[questionIndex]["correct"];
+  const wrongAnswer = userAnswer !== questions[questionIndex]["correct"];
+  const nextQuize = questionIndex !== questions.length - 1;
+  const endQuize = questionIndex === questions.length - 1;
+  let score = questions[questionIndex]["score"];
+
+  if (rightAnswer && nextQuize) {
+    console.log(questionIndex);
+    if (questionIndex >= 2 && questionIndex < 3) {
+      console.log("$1000");
+    }
+    questionIndex++;
+    clearPage();
+    showQuestion();
+  } else if (wrongAnswer) {
+    console.log("wrong");
+  } else if (endQuize) {
+    console.log("goodbye");
+    clearPage();
+    showResults();
+  }
+}
+
+function showResults() {
+  let score = questions[questionIndex]["score"];
+  console.log("showRes");
+  console.log(score);
+  const resultTemplate = `
+          <h2 class="title">%title%</h2>
+          <h3 class="summary">%message%</h3>
+          <p class="result">%result%</p>
+        `;
+  if (questionIndex >= 1 && questionIndex < 2) {
+    title = "WINNER!!!";
+    message = "NOT BAD!!!";
+    result = "$1000";
+    console.log("$1000");
+  } else if (questionIndex >= 2 && questionIndex < 3) {
+    title = "WINNER!!!";
+    message = "GOOD JOB!!!";
+    result = "$32000";
+    console.log("$32000");
+  } else if ((questionIndex = 4)) {
+    title = "WINNER!!!";
+    message = "YOU ARE RICH!!!";
+    result = "$1000000";
+    console.log("$1000000");
+  }
+  const finalMessage = resultTemplate
+    .replace("%title%", title)
+    .replace("%message%", message)
+    .replace("%result%", result);
+  quiz.innerHTML = finalMessage;
+}
+
+// submitBtn.onclick = checkAnswer;
---
+++ js/questions.js
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    question: "Какой язык работает в браузере?",
+    answers: ["Java", "C", "Python", "JavaScript"],
+    correct: 4,
+    score: "$100",
+  },
+  {
+    question: "Что означает CSS?",
+    answers: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    correct: 2,
+    score: "$200",
+  },
+  {
+    question: "Что означает HTML?",
+    answers: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    correct: 1,
+    score: "$300",
+  },
+  {
+    question: "В каком году был создан JavaScript?",
+    answers: ["1996", "1995", "1994", "все ответы неверные"],
+    correct: 2,
+    score: "$500",
+  },
+];
