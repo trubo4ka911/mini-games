fd454ab0a14bcb8880bd614bdf577abf2fe1c98a
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2023-01-26 08:07:49 +0200

    game

---
+++ js/calculator.js
@@ -0,0 +1,88 @@
+let a = "";
+let b = "";
+let sign = "";
+let finish = false;
+
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "%"];
+const action = ["-", "+", "x", "/"];
+
+const out = document.querySelector(".calc-screen p");
+
+document.querySelector(".plus-minus").onclick = changeSign;
+function changeSign() {
+  a = -a;
+  return (out.textContent = a);
+}
+
+function clearAll() {
+  a = "";
+  b = "";
+  sign = "";
+  finish = false;
+  out.textContent = 0;
+}
+
+document.querySelector(".ac").onclick = clearAll;
+
+document.querySelector(".buttons").onclick = (event) => {
+  if (!event.target.classList.contains("btn")) return;
+  if (event.target.classList.contains("ac")) return;
+  out.textContent = "";
+
+  const key = event.target.textContent;
+  document.querySelector(".percent").onclick = percentNum;
+  function percentNum() {
+    a = parseFloat(a / 100);
+    console.log(a);
+    return (out.textContent = parseFloat(a));
+  }
+  if (digit.includes(key)) {
+    if (b === "" && sign === "") {
+      a += key;
+      out.textContent = a;
+    } else if (a !== "" && b !== "" && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+    } else {
+      b += key;
+      out.textContent = b;
+    }
+    console.log(a, b, sign);
+    return;
+  }
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.log(sign);
+    return;
+  }
+  if (key === "=") {
+    if (b === "") b = a;
+
+    switch (sign) {
+      case "+":
+        a = +a + +b;
+        break;
+      case "-":
+        a = a - b;
+        break;
+      case "x":
+        a = a * b;
+        break;
+      case "/":
+        if (b === "0") {
+          out.textContent = "Error";
+          a = "";
+          b = "";
+          sign = "";
+          return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
+    out.textContent = a;
+    console.table(a, b, sign);
+  }
+};
--- js/millionaire.js
+++ js/millionaire.js
@@ -5,6 +5,7 @@ const getAnswer = document.querySelector("#list");
 const submitBtn = document.querySelector("#submit");
 const newGameBtn = document.querySelector("#submit-new");
 const fiftyFiftyBtn = document.querySelector(".fifty-fifty");
+const callBtn = document.querySelector(".call-friend");
 
 let questionIndex = 0;
 clearPage();
@@ -18,18 +19,69 @@ newGameBtn.onclick = () => history.go();
 
 // 50/50
 fiftyFiftyBtn.onclick = hideAnswers;
+let checkFlag = 0;
 
-const randomNumHelperFunc = (num) => Math.floor(Math.random() * num);
 function hideAnswers() {
-  let answer = questions[questionIndex]["answers"];
-  let correct = questions[questionIndex]["correct"];
+  let correct = questions[questionIndex]["correct"] - 1;
+  fiftyFiftyBtn.classList.add("hidden");
+  disableElement(correct);
+}
 
-  for (el of answer) {
-    // console.log(el);
-    if (answer.indexOf(el) + 1 !== correct) {
-      document.querySelector("#submit").classList.add("hidden");
+function disableElement(correct) {
+  do {
+    allAnswer = getRandomArbitrary(0, 4);
+    let elementForHide = document.querySelector("#list").childNodes[allAnswer];
+    if (allAnswer === correct) {
+      continue;
     }
-  }
+    if (!elementForHide.classList.contains("hidden")) {
+      elementForHide.classList.add("hidden");
+      checkFlag++;
+    }
+  } while (checkFlag < 2);
+}
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// call
+callBtn.onclick = call;
+let checkFlag2 = 0;
+
+function call() {
+  let correct = questions[questionIndex]["correct"] - 1;
+  callBtn.classList.add("hidden");
+  changeElement(correct);
+}
+// let answer = questions[questionIndex]["answers"];
+// let correct = questions[questionIndex]["correct"];
+// for (el of answer) {
+//   // console.log(el);
+//   if (answer.indexOf(el) + 1 === correct) {
+//     console.log(el);
+//     submitBtn.classList.add("help-color");
+//     console.log(submitBtn.classList.add("help-color"));
+//   }
+// }
+// }
+function changeElement(correct) {
+  // allAnswer = getRandomArbitrary(0, 4);
+  // let elementForHide = document.querySelector("#list").childNodes[allAnswer];
+  // if (allAnswer !== correct) {
+  //   elementForHide.classList.add("help-color");
+  //   console.log(elementForHide);
+  // }
+  do {
+    allAnswer = getRandomArbitrary(0, 4);
+    let elementForHide = document.querySelector("#list").childNodes[allAnswer];
+    if (allAnswer === correct) {
+      continue;
+    }
+    if (!elementForHide.classList.contains("help-color")) {
+      elementForHide.classList.add("help-color");
+      checkFlag2++;
+    }
+  } while (checkFlag2 < 3);
 }
 
 function showQuestion() {
